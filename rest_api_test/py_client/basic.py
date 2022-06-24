13c768ae3da312b57203d3e5a3377dcecf4e506e
anatoliii123 <64612850+anatoliii123@users.noreply.github.com>
2022-06-24 15:58:57 -0500

    training

--- rest_api_test/py_client/basic.py
+++ rest_api_test/py_client/basic.py
@@ -1,4 +1,25 @@
 import requests
 
 endpoint = "https://httpbin.org/status/200"
-endpoint = "https://httpbin.org"
+endpoint = "https://httpbin.org/"
+
+get_response = requests.get(endpoint) #HTTP REQUEST
+print(get_response.text) #print raw text response
+
+# HTTP REQUEST WILL GIVE YOU ----> HTML
+# REST API REQUEST -----> JSON(XML)
+
+
+
+# JAVA SCRIPT OBJECT NOTATION:
+#
+# "data": "",
+# "files": {},
+# "form": {},
+# "headers": {},
+# "JSON": null,
+# "method": "GET",
+# "origin": "192.63.174.213",
+# "url": "https://httpbin.org/anything"
+#
+#JAVA SCRIPT OBJECT NOTATION:
